8c37e3edfc765b5058d0db30fe0b15fb86341759
Waris <warisareshi@gmail.com>
2024-11-19 15:28:39 +0530

    fix: minor fix

--- src/components/ui/rainbow-button.tsx
+++ src/components/ui/rainbow-button.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 
 import { cn } from "@/lib/utils";
-interface RainbowButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export function RainbowButton({
   children,
   className,
   ...props
-}: RainbowButtonProps) {
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       className={cn(
@@ -23,7 +21,7 @@ export function RainbowButton({
         // dark mode colors
         "dark:bg-[linear-gradient(#fff,#fff),linear-gradient(#fff_50%,rgba(255,255,255,0.6)_80%,rgba(0,0,0,0)),linear-gradient(90deg,hsl(var(--color-1)),hsl(var(--color-5)),hsl(var(--color-3)),hsl(var(--color-4)),hsl(var(--color-2)))]",
 
-        className,
+        className
       )}
       {...props}
     >
