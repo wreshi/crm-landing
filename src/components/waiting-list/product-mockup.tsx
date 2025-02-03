3cd83673105d4a9be77afc9ad737ef15353baca5
Waris <warisareshi@gmail.com>
2025-02-03 14:31:13 +0530

    fix: minor fix

--- src/components/waiting-list/product-mockup.tsx
+++ src/components/waiting-list/product-mockup.tsx
@@ -1,13 +1,8 @@
-"use client";
-import { useTheme } from "next-themes";
 import Image from "next/image";
-import React, { Suspense } from "react";
-import { ProductMockupImage } from "../../../public/product-mockup-image";
-import { BorderBeam } from "../ui/border-beam";
+import { Suspense } from "react";
 import { Loader2 } from "lucide-react";
 
 export function ProductMockup() {
-  const { theme } = useTheme();
   return (
     <section className="hidden md:block  md:px-56 lg:px-64 px-10">
       <div className="flex-col">
@@ -22,11 +17,7 @@ export function ProductMockup() {
             className="w-full h-full"
           />
         </Suspense>
-        {/* <BorderBeam size={300} duration={12} delay={9} className="" /> */}
       </div>
-      {/* <div className="bg-muted/40 relative flex-col items-center justify-center overflow-hidden rounded-lg w-full h-full">
-        {/* <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-        </span> */}
     </section>
   );
 }
